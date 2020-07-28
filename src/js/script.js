b1d0bdecbc62b9913adafbaae5c1bf5d0d84ff5d
Anastasia <a.maryina@yandex.ru>
2020-07-28 16:50:37 +0300

    added animation and pagination

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
-//welcome screen hiding
+const enterScreen = document.getElementById("enterScreen");
+const header = document.getElementById("header");
+const blog = document.getElementById("blog");
+const blogSecond = document.getElementById("blogSecond");
+const footer = document.getElementById("footer");
+const mainLogo = document.getElementById("mainLogo");
+const moreButton = document.getElementById("moreButton");
+const loadingBlock = document.getElementById("loading");
+
+function removeClass(element, nameOfClass) {
+    element.classList.remove(nameOfClass);
+};
+
+function addClass(element, nameOfClass) {
+    element.classList.add(nameOfClass);
+}
+
+//welcome screen hiding (not used)
 
 function toHide() {
-    const enterScreen = document.getElementById("enterScreen");
     if (enterScreen.style.display === "none") {
         enterScreen.style.display = "block";
     } else {
@@ -10,13 +26,42 @@ function toHide() {
 }
 
 
-//welcome screen animation (slide to top)
+//welcome screen animation (fadeOutUp)
 
-function addHideAnimation() {
-    let name, arr;
-    name = "enterOverlay_animated";
-    arr = enterScreen.className.split(" ");
-    if (arr.indexOf(name) == -1) {
-        enterScreen.className += " " + name;
-    };
+function addFadeOutUpAnimation() {
+    addClass(enterScreen, "animate__animated");
+    addClass(enterScreen, "animate__fadeOutUp");
+    removeClass(header, "notDisplayed");
+    removeClass(blog, "notDisplayed");
+    removeClass(footer, "notDisplayed");
 }
+
+
+//logo hover animation
+
+window.onload = function () {
+    let a = document.querySelector('#mainLogo');
+    a.onmouseover = function(e) {
+        addClass(mainLogo, "animate__animated");
+        addClass(mainLogo, "animate__shakeY");
+    };
+    a.onmouseout = function(e) {
+        removeClass(mainLogo, "animate__animated");
+        removeClass(mainLogo, "animate__shakeY");
+    };
+  }
+
+  //loading of content by clicking the button
+
+  function loadContent() {
+      addClass(moreButton, "animate__animated");
+      addClass(moreButton, "animate__fadeOut");
+      addClass(loadingBlock, "notDisplayed");
+      removeClass(blogSecond, "notDisplayed");
+      addClass(blogSecond, "animate__animated");
+      addClass(blogSecond, "animate__fadeInUp");
+      window.scrollBy({
+        top: 500,
+        behavior: 'smooth'
+      });
+  }
